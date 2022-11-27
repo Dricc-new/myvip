d5fcaff2232389740e35b57efe093169111e5c87
Dricc-new <diegoramoncc17@gmail.com>
2022-11-27 15:47:44 -0500

    trabajando en adminlte

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -37,7 +37,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/web.php'));
             
             Route::middleware('web','auth')
-                ->prefix('admin')
+                ->prefix('admin')->name('admin')
                 ->group(base_path('routes/admin.php'));
             
             Route::middleware('web')
