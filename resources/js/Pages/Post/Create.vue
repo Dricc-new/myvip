ef6b7e6630d1082707a1dbd99df2c09101a92ad0
daironjm <daironjm7@gmail.com>
2022-11-28 11:07:51 -0500

    Transformaciones con error en  Post/Create

--- resources/js/Pages/Post/Create.vue
+++ resources/js/Pages/Post/Create.vue
@@ -1,7 +1,10 @@
 
 <template>
-    <form method="post">
-        <input type="file" name="" id="">
-        <button type="submit">Publicar</button>
-    </form>
+    <div>
+        <form method="post">
+            <textarea name="" id="" cols="30" rows="10" placeholder="@{{ placeholder }}"></textarea>
+            <input type="file" name="" id="">
+            <button type="submit">Publicar</button>
+        </form>
+    </div>
 </template>
